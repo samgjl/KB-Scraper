67768eac0bb9db08613a9f5cc0e44ecaa4c0f5cf
Sam Johnson-Lacoss <99389146+samgjl@users.noreply.github.com>
2024-03-29 00:06:52 -0500

    Object-Orientation

--- kb_scraper.py
+++ kb_scraper.py
@@ -1,72 +1,89 @@
 import sys
-import requests
-import csv # for debugging
-from bs4 import BeautifulSoup
+import pandas
+from tqdm import tqdm
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-
 import getpass # for password input
 
-# from selenium.webdriver.firefox.service import Service #? For Firefox browsers
-from selenium.webdriver.chrome.service import Service #? For Chrome browsers
-
-sys.path.append("./chromedriver_win64") #? Add chromedriver to path
-
-
-
-def get_driver(service = "chrome") -> webdriver.Chrome | webdriver.Firefox:
-    if service.lower() == "firefox":
-        driver = webdriver.Firefox()
-    else:
-        driver = webdriver.Chrome()
-    return driver
-
-
-# testing for now
-def log_in(username, password) -> webdriver.Chrome | webdriver.Firefox:
-    # login url for KB:
-    url = r"https://stolafcarleton.teamdynamix.com/SBTDClient/2092/Carleton/Login.aspx?ReturnUrl=%2fSBTDClient%2f2092%2fCarleton%2fHome%2f"
-    
-    # Cleaning username:
-    if "@carleton.edu" != username[-13:]:
-        username += "@carleton.edu"
+class KBScraper:
+    def __init__(self, username: str = "", password: str = "", headless: bool = False):
+        # Cleaning username:
+        if len(username) < 14 or "@carleton.edu" != username[-13:]:
+            username += "@carleton.edu"
+        self.username = username
+        self.password = password
+        self.driver = self.get_driver(headless=headless)
         
-    # Load driver:  
-    driver = get_driver()
-    driver.get(url)
-    driver.maximize_window()
     
-    # Log in:
-    driver.find_element(By.ID, "txtUserName").send_keys(username)
-    driver.find_element(By.ID, "txtPassword").send_keys(password)
-    driver.find_element(By.ID, "btnSignIn").click()
-    
-    return driver
+    def get_driver(self, service: str = "chrome", headless: bool = False) -> webdriver.Chrome | webdriver.Firefox:        
+        if service.lower() == "firefox":
+            options = webdriver.FirefoxOptions()
+            if headless:
+                options.add_argument("--headless")
+                options.add_argument("--log-level=3")
+            self.driver = webdriver.Firefox(options=options)
+        elif service.lower() == "chrome":
+            options = webdriver.ChromeOptions()
+            if headless:
+                options.add_argument("--headless")
+                options.add_argument("--log-level=3")
+            self.driver = webdriver.Chrome(options=options)
+        else:
+            raise ValueError("Service must be either 'firefox' or 'chrome'.")
+        return self.driver
 
-# True if deleted, False if not.
-def delete_article(driver: webdriver.Chrome | webdriver.Firefox, article_id: int, base_url: str = None) -> bool:
-    if not base_url:
-        base_url = r"https://stolafcarleton.teamdynamix.com/SBTDClient/2092/Carleton/KB/EditDetails"
-    
-    driver.get(base_url + f"?ID={article_id}")
+
+    # testing for now
+    def log_in(self, username, password) -> webdriver.Chrome | webdriver.Firefox:
+        # login url for KB:
+        url = r"https://stolafcarleton.teamdynamix.com/SBTDClient/2092/Carleton/Login.aspx?ReturnUrl=%2fSBTDClient%2f2092%2fCarleton%2fHome%2f"
+        self.driver.get(url) 
+        self.driver.maximize_window()
         
-    delete = driver.find_element(By.ID, "ctl00_ctl00_ctl00_cpContent_cpContent_btnDeleteArticle")
-    # Are we allowed/able to delete?
-    if not delete:
-        return False
-    # Click delete:
-    delete.click()
-    driver.switch_to.alert.accept()
-    time.sleep(5)
-    return True
-            
+        # Log in:      
+        self.driver.find_element(By.ID, "txtUserName").send_keys(username)
+        self.driver.find_element(By.ID, "txtPassword").send_keys(password)
+        self.driver.find_element(By.ID, "btnSignIn").click()
+        
+        return self.driver
 
-def main(username, password):
-    driver = log_in(username, password)
-    delete_article(driver, 143859)
-    
-    
+    # True if deleted, False if not.
+    def delete_article(self, article_id: int, base_url: str = "https://stolafcarleton.teamdynamix.com/SBTDClient/2092/Carleton/KB/EditDetails") -> bool:
+        
+        self.driver.get(base_url + f"?ID={article_id}")
+        
+        # Make sure we are allowed to delete:
+        try:    
+            delete = self.driver.find_element(By.ID, "ctl00_ctl00_ctl00_cpContent_cpContent_btnDeleteArticle")
+            # Click delete:
+            delete.click()
+            self.driver.switch_to.alert.accept()
+            return True
+        except:
+            print(f"Article {article_id} not deletable.")
+            return False
+                
+
+    def example(self):
+        self.log_in(self.username, self.password)
+        successes = self.delete_from_csv("Articles-03-27-2024.csv")
+        self.dict_to_file(successes, "Deletion-Results.csv")
+        # self.delete_article(150038)
+        
+    def dict_to_file(self, dictionary, path) -> None:
+        with open(path, "w") as f:
+            f.write("Article ID, Deleted?\n")
+            f.write("\n".join([f"{key}, {value}" for key, value in dictionary.items()]))
+        
+    def delete_from_csv(self, csv_path):  
+        df = pandas.read_csv(csv_path)
+        successes = {}
+        for article_id in tqdm(df["ID"][:10]):
+            successes[article_id] = self.delete_article(article_id)
+        
+        return successes
+
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
@@ -77,4 +94,5 @@ if __name__ == "__main__":
     username = input("Carleton username: ")
     password = getpass.getpass("Carleton password: ")
     
-    main(username, password)
+    scraper = KBScraper(username=username, password=password)
+    scraper.example()
